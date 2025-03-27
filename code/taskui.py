76d0385852c6adbf188b3a94b9c2c29d3999be3e
LavraSource <lavriket@gmail.com>
2025-03-27 09:51:39 +0800

    uhhhhh

--- code/taskui.py
+++ code/taskui.py
@@ -11,7 +11,9 @@ from reverser import Reverser
 from scrollframe import ScrollFrame
 
 class TaskUI:
-
+    def quit(self):
+        self.frame.destroy()
+        #self.cmd()
     def advance_step(self):
         if False not in self.completed:
             showinfo(message="Вы завершили урок!")
@@ -34,8 +36,10 @@ class TaskUI:
         img = img.subsample(w,w)
         return img
     def __init__(self, task_number, root):
-
-
+        print("huh")
+        for widget in root.winfo_children():
+            widget.destroy()
+        #self.cmd = on_exit
         # Get Task1.json
         current_dir = os.path.dirname(os.path.abspath(__file__))
         root_dir = os.path.dirname(current_dir)
@@ -56,13 +60,13 @@ class TaskUI:
         self.checker = Checker(self.tasks, self.current_step)
         self.reverser = Reverser(self.tasks, self.current_step)
 
-        frame = Frame(root)
-        frame.pack(fill="both", expand=True)
-        frame.rowconfigure(1, weight=1)
-        frame.columnconfigure(0, weight=1)
-        task_frame = Frame(frame, relief=RAISED, pady=5, padx=3)
+        self.frame = Frame(root)
+        self.frame.pack(fill="both", expand=True)
+        self.frame.rowconfigure(1, weight=1)
+        self.frame.columnconfigure(0, weight=1)
+        task_frame = Frame(self.frame, relief=RAISED, pady=5, padx=3)
         task_frame.grid(column=0, row = 0,sticky = "nwse")
-        a_frame = ScrollFrame(frame)
+        a_frame = ScrollFrame(self.frame)
         a_frame.grid(column=0, row=1, sticky="nwse")
         main_frame = Frame(a_frame.viewPort)
         main_frame.configure(bg = "white")
@@ -71,7 +75,7 @@ class TaskUI:
         self.task_description = Label(main_frame, text=self.tasks[self.current_step][0], wraplength=500, bg="white", justify="left")
         self.img = PhotoImage(file = self.tasks[self.current_step][3]).subsample(1,1)
         self.task_image = Label(main_frame, image = self.img)
-        self.check_button = Button(frame, text = "Проверить", command=self.check, bg = "light green")
+        self.check_button = Button(self.frame, text = "Проверить", command=self.check, bg = "light green")
         for i in range(len(self.tasks)):
             cmd = partial( self.set_step, i)
             self.task_buttons.append(Button(task_frame, text = str(i+1), command=cmd, width = 2))
